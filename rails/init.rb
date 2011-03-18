eb399862fedc3102b2a35b591ea86aaf994c586f
James Hetherington <jamespjh@googlemail.com>
2011-03-18 15:14:47 +0000

    Clean up rails init

--- rails/init.rb
+++ rails/init.rb
@@ -1,12 +1,3 @@
-require 'amee'
-require 'amee/rails'
+require 'amee-data-abstraction.rb'
 
-# Load config/amee.yml
-amee_config = "#{RAILS_ROOT}/config/amee.yml"
-if File.exist?(amee_config)
-  # Load config
-  $AMEE_CONFIG = YAML.load_file(amee_config)[RAILS_ENV]
-end
-
-# Add AMEE extensions into ActiveRecord::Base
-ActiveRecord::Base.class_eval { include AMEE::Rails } if Object.const_defined?("ActiveRecord")
+AMEE::DataAbstraction.connection=AMEE::Rails.connection
