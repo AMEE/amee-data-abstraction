38dbfcf54b64d21bad6d0621da46f9e6ef50e2aa
James Hetherington <jamespjh@googlemail.com>
2011-03-21 16:20:23 +0000

    Two specs for core extensions

---
+++ spec/core-extensions/class_spec.rb
@@ -0,0 +1,25 @@
+require File.dirname(File.dirname(__FILE__)) + '/spec_helper.rb'
+
+ class Myclass
+     attr_property :prop
+   end
+
+describe Class do
+  before :all do
+    @it=Myclass.new
+  end
+ it 'can have a property defined' do
+  @it.prop 5
+  @it.prop.should eql 5
+ end
+ it 'can have a prop unset' do
+   @it.prop 5
+   @it.prop nil
+   @it.prop.should be_nil
+ end
+ it 'does not unset by query' do
+   @it.prop 5
+   @it.prop
+   @it.prop.should_not be_nil
+ end
+end
---
+++ spec/core-extensions/proc_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(File.dirname(__FILE__)) + '/spec_helper.rb'
+
+describe Proc do
+  before :all do
+    @it=Proc.new{|x|x.is_a? Float}
+  end
+ it 'uses call as ===' do
+  (@it===3.4).should be_true
+  (@it===7).should be_false
+  (@it==='hello').should be_false
+ end
+end
