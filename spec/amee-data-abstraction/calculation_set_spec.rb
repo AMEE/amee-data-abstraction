d3795eccb42c430fa26f4ac8e326f640d85362ca
James Hetherington <jamespjh@googlemail.com>
2011-03-24 15:35:55 +0000

    CalculationSet test

--- spec/amee-data-abstraction/calculation_set_spec.rb
+++ spec/amee-data-abstraction/calculation_set_spec.rb
@@ -1,1 +1,23 @@
 require File.dirname(File.dirname(__FILE__)) + '/spec_helper.rb'
+class CalculationSet
+  def call_me
+    #stub, because flexmock doesn't work for new instances during constructor
+    @@called=true
+  end
+  cattr_accessor :called
+end
+describe CalculationSet do
+  it 'can create an instance' do
+    ElectricityAndTransport.should be_a CalculationSet
+  end
+  it 'can access a calculation by key' do
+    ElectricityAndTransport[:transport].should be_a PrototypeCalculation
+  end
+  it 'can construct a calculation' do
+    CalculationSet.new {calculation {label :mycalc}}[:mycalc].should be_a PrototypeCalculation
+  end
+  it 'can be initialized with a DSL block' do
+    CalculationSet.new {call_me}
+    CalculationSet.called.should be_true
+  end
+end
--- spec/fixtures/electricity_and_transport.rb
+++ spec/fixtures/electricity_and_transport.rb
@@ -18,4 +18,31 @@ ElectricityAndTransport=AMEE::DataAbstraction::CalculationSet.new {
       path :default
     }
   }
+
+  calculation {
+    name 'transport'
+    label :transport
+    path '/transport/car/generic'
+
+    drill {
+      path 'fuel'
+      label :fuel
+      name 'Fuel Type'
+    }
+    drill {
+      path 'size'
+      label :size
+      name 'Vehicle Size'
+    }
+    profile {
+      path 'distance'
+      label :distance
+      name 'Distance Driven'
+    }
+    output {
+      label :co2
+      path :default
+      name 'Carbon Dioxide'
+    }
+  }
 }
