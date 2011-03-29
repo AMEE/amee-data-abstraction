447ec24bcb80cf00125d94fd3ad2ef9ff8eefcaf
James Hetherington <jamespjh@googlemail.com>
2011-03-29 17:00:07 +0100

    Vbump, feature complete

--- lib/amee-data-abstraction/version.rb
+++ lib/amee-data-abstraction/version.rb
@@ -2,7 +2,7 @@ module AMEE
   module DataAbstraction
     module VERSION #:nodoc:
       MAJOR = 0
-      MINOR = 3
+      MINOR = 4
       TINY  = 0
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
