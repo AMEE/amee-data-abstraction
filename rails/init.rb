0406233625eb844531785de3271f6011cbd06db1
James Smith <james@floppy.org.uk>
2011-05-20 17:56:42 +0100

    lazy evaluation for connection. AC-73

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,10 @@
 require 'amee-data-abstraction.rb'
 
-AMEE::DataAbstraction.connection=AMEE::Rails.connection
+module AMEE
+  module DataAbstraction
+    # Override the connection accessor to provide the global connection in rails apps
+    def connection
+      AMEE::Rails.connection
+    end
+  end
+end
