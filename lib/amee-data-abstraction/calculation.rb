64b7a2bb246c814dd65b36b2118e70b61cd96963
George Palmer <george.palmer@gmail.com>
2011-08-04 15:46:01 +0100

    Improvement to inspect method output

--- lib/amee-data-abstraction/calculation.rb
+++ lib/amee-data-abstraction/calculation.rb
@@ -129,7 +129,7 @@ module AMEE
 
       # Prettyprint a string representation of <tt>self</tt>, together with associated terms
       def inspect
-        elements = {:label => label.inspect, :terms => terms.map{|t| "<Term label:#{t.label}, value:#{t.value.inspect}>"}}
+        elements = {:label => label.inspect, :terms => terms.map{|t| "<#{t.class.name.demodulize} label:#{t.label}, value:#{t.value.inspect}>"}}
         attr_list = elements.map {|k,v| "#{k}: #{v}" } * ', '
         "<#{self.class.name} #{attr_list}>"
       end
